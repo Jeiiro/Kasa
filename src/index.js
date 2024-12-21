f2a58fd22fc849e9aef8dab4f085dc17770085ec
Dasilva Tadeu Jérémy <dasilvatjeremy@gmail.com>
2024-12-21 18:02:55 +0100

    COmmit for correction after oral

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.scss";
-import App from "./components/App.js";
-import Error from "./components/Error.js";
-import Infos from "./components/Infos.js";
-import Logement from "./components/Logements.js";
+import App from "./pages/App.js";
+import Error from "./pages/Error.js";
+import Infos from "./pages/Infos.js";
+import Logement from "./pages/Logements.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 
